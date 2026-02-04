d11a1006b73eaa17181b0f578e87c0888427b90d
sahil katara <sahilkatara55@gmail.com>
2026-02-04 23:53:57 +0530

    first commit

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student,Product
+# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Product)
